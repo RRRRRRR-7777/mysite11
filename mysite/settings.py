f92d32136e30838c2ad897c83042d9c903bc9e57
RRRRRRR-7777 <yr1919@icloud.com>
2022-06-03 11:39:41 +0900

    Django8課題 完成

--- mysite/settings.py
+++ mysite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'ja'
 
 TIME_ZONE = 'Asia/Tokyo'
 
